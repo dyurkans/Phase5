1788cbd4f256b80ed66f61a25986f8e3c800ad57
Dima Yurkanskiy <dyurkans@cmu.edu>
2013-05-05 01:04:21 -0400

    working on authent

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Karate67272::Application.routes.draw do
 
-  match 'user/edit' => 'users#edit', :as => :edit_current_user
-
   # Authentication routes
-  match 'user/edit' => 'users#edit', :as => :edit_user
+  match 'user/edit' => 'users#edit', :as => :edit_current_user
   match 'signup' => 'users#new', :as => :signup
   match 'logout' => 'sessions#destroy', :as => :logout
   match 'login' => 'sessions#new', :as => :login
