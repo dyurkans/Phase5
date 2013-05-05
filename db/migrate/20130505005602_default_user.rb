bdfa9d01331542cadb70c2cade59fe0438729b3e
Dima Yurkanskiy <dyurkans@cmu.edu>
2013-05-05 03:34:47 -0400

    Fixed authentication bug. passing features levels 1-4.

--- db/migrate/20130505005602_default_user.rb
+++ db/migrate/20130505005602_default_user.rb
@@ -1,7 +1,7 @@
 class DefaultUser < ActiveRecord::Migration
   def up
     admin = User.new
-    admin.email = "admin@example.com"
+    admin.email = "profh@cmu.edu"
     admin.password = "secret"
     admin.password_confirmation = "secret"
     admin.role = "admin"
