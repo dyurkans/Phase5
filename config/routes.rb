c8d74bec599383ecd0f940a8cca1e36d00698282
Dima Yurkanskiy <dyurkans@cmu.edu>
2013-05-04 22:19:17 -0400

    Added user and sessions controllers/views. Working on authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Karate67272::Application.routes.draw do
 
+  match 'user/edit' => 'users#edit', :as => :edit_current_user
+
+  # Authentication routes
+  match 'user/edit' => 'users#edit', :as => :edit_user
+  match 'signup' => 'users#new', :as => :signup
+  match 'logout' => 'sessions#destroy', :as => :logout
+  match 'login' => 'sessions#new', :as => :login
+  resources :sessions
+  resources :users
+
   # Generated routes
   resources :events
   resources :registrations
